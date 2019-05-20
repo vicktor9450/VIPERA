3c8bf4f693b57a96cfabf8907f69df030b0431ef
Tibor Bödecs <mail.tib@gmail.com>
2019-05-20 18:04:33 +0200

    version & result info

--- Sources/vipera/main.swift
+++ Sources/vipera/main.swift
@@ -1,6 +1,13 @@
 import Foundation
 import Dir
 
+let version = "1.0.2"
+
+if CommandLine.arguments.contains("--version") {
+    print("VIPERA code generator - v\(version)")
+    exit(0)
+}
+
 guard CommandLine.arguments.count > 1 else {
     print("You have to to provide a module name as the first argument.")
     exit(-1)
@@ -8,7 +15,7 @@ guard CommandLine.arguments.count > 1 else {
 
 let moduleName = CommandLine.arguments[1].capitalizedFirstLetter()
 let scriptName = URL(fileURLWithPath: CommandLine.arguments.first!).lastPathComponent
-var templateName = "Default"
+var templateName = "default"
 if CommandLine.arguments.count > 2 {
     templateName = CommandLine.arguments[2]
 }
@@ -33,7 +40,13 @@ let params = [
 do {
     let moduleDir = try currentDir.add(moduleName)
     try createModuleComponent(from: templateDir.url, to: moduleDir.url)
+    print("🐍 VIPER module `\(moduleName)` is ready to use. (based on the `\(templateName)` template)")
 }
 catch {
     print(error.localizedDescription)
+}
+
+
+if templateName == "default" || templateName == "generic" {
+    print("\n⚠️  In order to use the module you'll need the following library:\n\n\thttps://github.com/corekit/viper\n")
 }
